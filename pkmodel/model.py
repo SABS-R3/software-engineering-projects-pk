f5ca04549b4275ca87842f33981471108eda88d9
Sixtine Dromigny <sixtine.dromigny@gmail.com>
2023-10-19 13:42:01 +0100

    issue #7 and #15 are resolved:parametrising and integration functions are inside Class Model with 2 children IV and SC, depending on which model the user wants to use

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -25,39 +25,53 @@ class IV(Model):
         super().__init__()
         self.parameters = parameters
 
-    def param(self, y, t):
+    def paramIV(self, y, t):
         Q_p1, V_c, V_p1, CL, X = self.parameters
         q_c, q_p1 = y
         transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
         dqc_dt = X - q_c / V_c * CL - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt]
+    
+    def integrate():
+        iv_instance = IV()
+        t_eval = np.linspace(0, 1, 1000)
+        y0 = [0.0, 0.0]
+        iv_instance.parameters = parameters
+        solution = odeint(iv_instance.paramIV, y0, t_eval)
+        q_c, q_p1 = solution.T
+        return np.array([q_c,q_p1])
 
+############TESTING IF CLASS WORKS
+# parameters = [0.7, 1, 2, 3, 4]
+# test1=IV.integrate()
+# print('test1',test1.shape)
 
-# Create an instance of the IV class
-iv_instance = IV()
+############INTEGRATION OUTSIDE THE CLASS
+# # Create an instance of the IV class
+# iv_instance = IV()
 
-# Define the time points at which you want to evaluate the solution
-t_eval = np.linspace(0, 1, 1000)
+# # Define the time points at which you want to evaluate the solution
+# t_eval = np.linspace(0, 1, 1000)
 
-# Initial conditions
-y0 = [0.0, 0.0]
+# # Initial conditions
+# y0 = [0.0, 0.0]
 
-# Parameters
-parameters = [0.7, 1, 2, 3, 4]
+# # Parameters
+# parameters = [0.7, 1, 2, 3, 4]
 
-# Set the parameters in the instance
-iv_instance.parameters = parameters
+# # Set the parameters in the instance
+# iv_instance.parameters = parameters
 
-# Solve the ODEs using odeint
-solution = odeint(iv_instance.param, y0, t_eval)
+# # Solve the ODEs using odeint
+# solution = odeint(iv_instance.paramIV, y0, t_eval)
 
-# Extract the results
-q_c, q_p1 = solution.T
+# # Extract the results
+# q_c, q_p1 = solution.T
 
 # Print the results
-print(q_c)
-print(q_p1)
+# print(q_c)
+# print(q_p1)
 
 
 class SC(Model):
@@ -66,41 +80,56 @@ class SC(Model):
         super().__init__()
         self.parameters = parameters
 
-    def param(self, y, t):
+    def paramSC(self, y, t):
         Q_p1, V_c, V_p1, CL, X, ka = self.parameters
-        q_c, q_p1, q0= y
-        dq0_dt = X - ka*q0
+        q_c, q_p1, q_0= y
+        dq0_dt = X - ka*q_0
         transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-        dqc_dt = ka*q0 - q_c / V_c * CL - transition
+        dqc_dt = ka*q_0 - q_c / V_c * CL - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt,dq0_dt]
 
+    def integrate():
+        sc_instance = SC()
+        t_eval = np.linspace(0, 1, 1000)
+        y0 = [0.0, 0.0, 0.0]
+        sc_instance.parameters = parameters
+        solution = odeint(sc_instance.paramSC, y0, t_eval)
+        q_c, q_p1, q_0 = solution.T
+        return np.array([q_c,q_p1,q_0])
 
-# Create an instance of the IV class
-sc_instance = SC()
 
-# Define the time points at which you want to evaluate the solution
-t_eval = np.linspace(0, 1, 1000)
+############TESTING IF CLASS WORKS
+# parameters = [0.7, 1, 2, 3, 4, 5]
+# test2=SC.integrate()
+# print('test2',test2.shape)
 
-# Initial conditions
-y0 = [0.0, 0.0,0]
+############INTEGRATION OUTSIDE THE CLASS
+# # Create an instance of the IV class
+# sc_instance = SC()
 
-# Parameters
-parameters = [0.7, 1, 2, 3, 4,7]
+# # Define the time points at which you want to evaluate the solution
+# t_eval = np.linspace(0, 1, 1000)
 
-# Set the parameters in the instance
-sc_instance.parameters = parameters
+# # Initial conditions
+# y0 = [0.0, 0.0,0]
 
-# Solve the ODEs using odeint
-solution = odeint(sc_instance.param, y0, t_eval)
+# # Parameters
+# parameters = [0.7, 1, 2, 3, 4,7]
 
-print(solution.shape)
+# # Set the parameters in the instance
+# sc_instance.parameters = parameters
 
-# Extract the results
-print(solution.T.shape)
-q_c, q_p1, q_0 = solution.T
+# # Solve the ODEs using odeint
+# solution = odeint(sc_instance.paramSC, y0, t_eval)
 
-# Print the results
-print(q_c.shape)
-print(q_p1.shape)
-print(solution.y[0, :])
+# print(solution.shape)
+
+# # Extract the results
+# # print(solution.T.shape)
+# q_c, q_p1, q_0 = solution.T
+
+# # Print the results
+# # print(q_c.shape)
+# # print(q_p1.shape)
+# # print(solution.y[0, :])
