8589c602fee6d46bce1efd70ac29462a07ffaa63
Sixtine Dromigny <sixtine.dromigny@gmail.com>
2023-10-19 15:34:44 +0100

    Rhiannon-s code added in solution.py in srd/pkmodel

--- src/pkmodel/solution.py
+++ src/pkmodel/solution.py
@@ -1,16 +1,27 @@
-#
-# Solution class
-#
+import numpy as np
+import matplotlib.pylab as plt
+import model
 
-class Solution:
-    """A Pharmokinetic (PK) model solution
+def visualise(modeldata):
+    time=np.linspace(0,1,1000)
+    fig, ax = plt.subplots()
+    # Plot q_c
+    plt.plot(time, modeldata[0,:], label = 'q_c')
+    # Plot q_p1
+    plt.plot(time, modeldata[1,:], label = 'q_p1')
+    # Plot q_0
+    if np.shape(modeldata) == (3, 1000):
+        plt.plot(time, modeldata[2,:], label = 'q_0')
+        plt.title('Subcutaneous model')
+    else:
+        plt.title('IV model')
+    plt.legend()
+    ax.set_ylabel('drug mass (ng)')
+    ax.set_xlabel('time [h]')
+    #plt.savefig('plot.png')
+    plt.show()
 
-    Parameters
-    ----------
+parameter=[1,2,3,4,5]
 
-    value: numeric, optional
-        an example paramter
-
-    """
-    def __init__(self, value=44):
-        self.value = value
+result=visualise(model.IV.integrate())
+print(result)
